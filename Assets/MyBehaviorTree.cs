f546a7d0ea18c6a1532edcd32e7ccff7be6b0ffe
Vineet Sepaha <Vineet Sepaha>
2016-10-30 04:05:35 -0400

    v

--- Assets/MyBehaviorTree.cs
+++ Assets/MyBehaviorTree.cs
@@ -5,39 +5,61 @@ using TreeSharpPlus;
 
 public class MyBehaviorTree : MonoBehaviour
 {
-	public Transform wander1;
-	public Transform wander2;
-	public Transform wander3;
-	public GameObject participant;
+    public Transform wander1;
+    public Transform wander2;
+    public Transform wander3;
+    //  public Animation a;
+    public GameObject participant;
 
-	private BehaviorAgent behaviorAgent;
-	// Use this for initialization
-	void Start ()
-	{
-		behaviorAgent = new BehaviorAgent (this.BuildTreeRoot ());
-		BehaviorManager.Instance.Register (behaviorAgent);
-		behaviorAgent.StartBehavior ();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	
-	}
+    private BehaviorAgent behaviorAgent;
+    // Use this for initialization
+    void Start()
+    {
+        behaviorAgent = new BehaviorAgent(this.BuildTreeRoot());
+        BehaviorManager.Instance.Register(behaviorAgent);
+        behaviorAgent.StartBehavior();
+    }
 
-	protected Node ST_ApproachAndWait(Transform target)
-	{
-		Val<Vector3> position = Val.V (() => target.position);
-		return new Sequence( participant.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
-	}
+    // Update is called once per frame
+    void Update()
+    {
 
-	protected Node BuildTreeRoot()
-	{
-		Node roaming = new DecoratorLoop (
-						new SequenceShuffle(
-						this.ST_ApproachAndWait(this.wander1),
-						this.ST_ApproachAndWait(this.wander2),
-						this.ST_ApproachAndWait(this.wander3)));
-		return roaming;
-	}
+    }
+
+    protected Node Body(string s)
+    {
+        Val<string> name = Val.V(() => s);
+        return new Sequence(participant.GetComponent<BehaviorMecanim>().ST_PlayBodyGesture(name, 4000));
+    }
+
+    protected Node Hand(string s)
+    {
+        Val<string> name = Val.V(() => s);
+        return new Sequence(participant.GetComponent<BehaviorMecanim>().ST_PlayHandGesture(name, 4000));
+    }
+
+    protected Node Face(string s)
+    {
+        Val<string> name = Val.V(() => s);
+        return new Sequence(participant.GetComponent<BehaviorMecanim>().ST_PlayFaceGesture(name, 4000));
+    }
+    protected Node ST_ApproachAndWait(Transform target)
+    {
+        Val<Vector3> position = Val.V(() => target.position);
+        return new Sequence(participant.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
+    }
+
+
+    protected Node BuildTreeRoot()
+    {
+        Node roaming = new DecoratorLoop(
+                        new Sequence(
+
+                        this.Face("HEADSHAKE"),
+                        this.ST_ApproachAndWait(wander1),
+                        this.Hand("CHEER"),
+                        this.ST_ApproachAndWait(wander2)));
+
+        return roaming;
+    }
 }
