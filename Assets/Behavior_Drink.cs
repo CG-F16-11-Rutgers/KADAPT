bdde469bc1b663c0f765b73f651a62e5636057d7
Albert Stein <albert.stein@rutgers.edu>
2016-10-30 14:49:13 -0400

    door in progress

--- Assets/Behavior_Drink.cs
+++ Assets/Behavior_Drink.cs
@@ -8,6 +8,7 @@ public class Behavior_Drink : MonoBehaviour {
     //public Transform wander2;
     //public Transform wander3;
     public GameObject participant;
+    public GameObject drink;
 
     private BehaviorAgent behaviorAgent;
     // Use this for initialization
@@ -19,7 +20,6 @@ public class Behavior_Drink : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-
     }
 
     protected Node ST_ApproachAndWait(Transform target) {
@@ -27,12 +27,12 @@ public class Behavior_Drink : MonoBehaviour {
         return new Sequence(participant.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
     }
 
-    protected Node ST_OpenDoor(Transform target) { //UNIMPLEMENTED
-        return null;
-    }
-
     protected Node ST_WaitRandom(long min, long max) {
         return new Sequence(new LeafWait((long)(((max - min) * UnityEngine.Random.value) + min)));
+    }
+
+    protected Node ST_PickupObject(GameObject target) {
+        return null;
     }
 
     protected Node ST_Sit() {
@@ -51,6 +51,7 @@ public class Behavior_Drink : MonoBehaviour {
     protected Node BuildTreeRoot() {
         Node roaming = 
             new Sequence(
+                //ST_PickupObject(drink),
                 ST_Sit(),
                 new DecoratorLoop(
                     new Sequence(
